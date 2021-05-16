e5f6c82fcb8d7166c3d80b255fa8c59642a9c2f0
Meet Patel <meetaiims@yahoo.com>
2021-05-16 03:10:37 -0400

    add canLaunchBrowser() method to SeleniumTest class

--- src/test/java/com/selenium/SeleniumTest.java
+++ src/test/java/com/selenium/SeleniumTest.java
@@ -1,11 +1,19 @@
 package com.selenium;
 
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
 	
   @Test
-  public void canLaunchWebBrowser(String url) {
+  public void canLaunchBrowser() {
 	  
+	  String url = "https://demoqa.com/";
+	  
+	  WebDriver driver = SeleniumDriverUtil.launchBrowser(url);
+	  assertEquals(driver.getCurrentUrl(), url);
+	  driver.quit();
   }
 }
