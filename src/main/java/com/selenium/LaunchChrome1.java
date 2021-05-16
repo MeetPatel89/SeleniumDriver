e3fb664b52138405e842fb0a3d5892c2adb903dd
Meet Patel <meetaiims@yahoo.com>
2021-05-16 01:19:28 -0400

    add new path for chromedriver within the project

--- src/main/java/com/selenium/LaunchChrome1.java
+++ src/main/java/com/selenium/LaunchChrome1.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchChrome1 {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "/home/nautilusshell/Documents/chromedriver/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com");
 		System.out.println(driver.getTitle());
-	}
+	}                                                                                                                                                   
 }
