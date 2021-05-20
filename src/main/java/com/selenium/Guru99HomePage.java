24868590d237784480e6bf232c1943a585fe86ff
Meet Patel <meetaiims@yahoo.com>
2021-05-19 23:38:48 -0400

    add Guru99HomePage class

---
+++ src/main/java/com/selenium/Guru99HomePage.java
@@ -0,0 +1,14 @@
+package com.selenium;
+
+import org.openqa.selenium.WebDriver;
+
+public class Guru99HomePage {
+	WebDriver driver;
+	
+	public Guru99HomePage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	//Get the user name from home page
+	public Strin
+}
